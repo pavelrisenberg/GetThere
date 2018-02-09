75def5453ee2945f68612a160950a79658ac4426
pavelrisenberg <me@pavelrisenberg.com>
2018-02-09 19:37:16 +0300

    Design updates and bug fixes

--- companion/gmaps.js
+++ companion/gmaps.js
@@ -12,7 +12,7 @@ GoogleMapsAPI.prototype.getRouteTiming = function(origin, destination, resultArr
   return new Promise(function(resolve, reject) {
     var url = "https://maps.googleapis.com/maps/api/distancematrix/json?";
     url += "&key=" + self.apiKey;
-    url += "&mode=driving&units=metric";
+    url += "&mode=driving&units=metric&departure_time=now&traffic_model=pessimistic";
     url += "&origins=" + origin;
     url += "&destinations=" + destination;
     console.log("Fetching URL: " + url);
@@ -22,7 +22,11 @@ GoogleMapsAPI.prototype.getRouteTiming = function(origin, destination, resultArr
       console.log("Got JSON response from server:" + JSON.stringify(json));
 
       for (let i = 0; i < json["rows"][0]["elements"].length; i++) {
-        var duration = (Number.parseInt(json["rows"][0]["elements"][i]["duration"]["value"]) / 60);
+        if(json["rows"][0]["elements"][i]["duration_in_traffic"]["value"]) {
+          var duration = (Number.parseInt(json["rows"][0]["elements"][i]["duration_in_traffic"]["value"]) / 60);          
+        } else {
+          var duration = (Number.parseInt(json["rows"][0]["elements"][i]["duration"]["value"]) / 60);          
+        }
         var distance = (Number.parseInt(json["rows"][0]["elements"][i]["distance"]["value"]));
         
         resultArray[i].duration = duration;
